128fe044b347c533546b9b2be6fa82f25559c521
envis10n <envis10n@envis10n.dev>
2024-04-28 23:49:08 -0500

    Init

---
+++ index.ts
@@ -0,0 +1,124 @@
+import axios, { Axios } from "axios";
+import Koa from "koa";
+import fs from "fs/promises";
+import { exportPlacefile, type Line, type Polygon, type IPlacefileOptions, Vec2, COLOR_TABLE } from "./placefile";
+
+const app = new Koa();
+
+interface IFeature {
+    type: "Feature";
+    geometry: {
+        type: string;
+        coordinates: Array<Array<[number, number]>>;
+    }
+    properties: {
+        status: "Draft" | "Test" | "System" | "Exercise" | "Actual";
+        areaDesc: string;
+        messageType: "Alert" | "Update" | "Cancel"
+        sent: string;
+        effective: string;
+        onset: string;
+        expires: string;
+        ends: string;
+        category: string;
+        severity: "Unknown" | "Minor" | "Moderate" | "Severe" | "Extreme";
+        certainty: "Unknown" | "Unlikely" | "Possible" | "Likely" | "Observed";
+        urgency: "Unknown" | "Past" | "Future" | "Expected" | "Immediate";
+        event: "Tornado Warning" | "Severe Thunderstorm Warning" | "Tornado Emergency" | "Tornado Watch" | "Severe Thunderstorm Watch" | "Special Weather Statement";
+        instruction: string;
+        NWSheadline: string[];
+        eventEndingTime: string[];
+        description: string;
+        headline: string;
+    }
+    [key: string]: any;
+}
+
+const request = axios.create({
+    baseURL: "https://api.weather.gov",
+    headers: {
+        "Accept": "application/geo+json",
+        "User-Agent": "(wx.envis10n.dev, envis10n+wx@envis10n.dev)"
+    }
+});
+
+let last_cache = 0;
+let last_write = 0;
+let is_locked = false;
+
+let alerts = (await fs.exists("alerts.json") ? JSON.parse(await fs.readFile("alerts.json", {encoding: "utf-8"})) : {});
+
+function buildPlacefile(): string {
+    if (Object.keys(alerts).length = 0) return "";
+    const features: IFeature[] = alerts.features;
+
+    const opts: IPlacefileOptions = {
+        title: `NWS Active Warnings ${(new Date()).toISOString()}`,
+        refreshMs: 8 * 60 * 1000,
+        threshold: 999,
+        geometry: [],
+    }
+
+    for (const feature of features.filter((f) => f.geometry != null)) {
+        // build polygons
+        const avg = new Vec2();
+        const coords = feature.geometry.coordinates.flat(1).map(([lon, lat]) => {
+            const v = new Vec2(lat,lon);
+            avg.add(v);
+            return v;
+        });
+        const centroid = `${avg.x / coords.length},${avg.y / coords.length}`;
+        let color = COLOR_TABLE[feature.properties.event];
+        let width = 4;
+        if (feature.properties.event.startsWith("Tornado") && feature.properties.certainty == "Observed") width = 8;
+        if (color == undefined) color = [255, 255, 255];
+        const line: Line = {
+            type: "Line",
+            hoverText: `${feature.properties.headline}\n\n${feature.properties.description}`,
+            width,
+            color,
+            points: coords
+        }
+        opts.geometry.push(line);
+    }
+    return exportPlacefile(opts);
+}
+
+let placefile = buildPlacefile();
+
+async function loop() {
+    try {
+        const res = await request.get("/alerts/active?event=Severe%20Thunderstorm%20Warning,Tornado%20Warning,Flood%20Warning,Special%20Weather%20Statement", {responseType: "json"});
+        const json = res.data;
+        alerts = JSON.parse(JSON.stringify(json));
+        await fs.writeFile("alerts.json", JSON.stringify(alerts));
+        placefile = buildPlacefile();
+    } catch (e) {
+        console.error("ERROR GETTING ALERTS:", e);
+        process.exit(1);
+    }
+}
+
+setInterval(() => {
+    if (is_locked) return;
+    if (Date.now() < last_cache) return;
+    is_locked = true;
+    loop().then(() => {
+        last_write = Date.now();
+        last_cache = last_write + 3 * 60 * 1000;
+        is_locked = false;
+    });
+}, 0);
+
+app.use(async ctx => {
+    const ts = Date.now();
+    if (placefile == "") ctx.status = 300;
+    else {
+        ctx.body = placefile;
+        ctx.status = 200;
+    }
+});
+
+app.listen(3525);
+
+console.log("Listening...");
---
+++ placefile.ts
@@ -0,0 +1,105 @@
+export class Vec2 {
+    public point: [number, number];
+    public get x(): number {
+        return this.point[0];
+    }
+    public set x(v: number) {
+        this.point[0] = v;
+    }
+    public get y(): number {
+        return this.point[1];
+    }
+    public set y(v: number) {
+        this.point[1] = v;
+    }
+    constructor();
+    constructor(x: number, y: number);
+    constructor(x = 0, y = 0) {
+        this.point = [x, y];
+    }
+    public toString(): string {
+        return `${this.x},${this.y}`;
+    }
+    public add(b: Vec2): this {
+        this.x += b.x;
+        this.y += b.y;
+        return this;
+    }
+    public sub(b: Vec2): this {
+        this.x -= b.x;
+        this.y -= b.y;
+        return this;
+    }
+    public length(): number {
+        return Math.abs(Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2)));
+    }
+}
+
+export const COLOR_TABLE: {[key: string]: [number, number, number]} = {
+    "Tornado Warning": [255, 0, 0],
+    "Severe Thunderstorm Warning": [255, 255, 0],
+    "Tornado Emergency": [255, 0, 255],
+    "Flood Warning": [0, 255, 0],
+    "Special Weather Statement": [255, 255, 204],
+};
+
+export type IPlacefileOptions = {
+    title: string;
+    threshold: number;
+    refreshMs: number;
+    defaultColor?: [number, number, number];
+    geometry: Array<Line | Polygon>;
+}
+
+export function exportPlacefile(opts: IPlacefileOptions): string {
+    const lines: string[] = [];
+    const refresh_total_sec = opts.refreshMs / 1000;
+    const refresh_mins = Math.floor(refresh_total_sec / 60);
+    const refresh_secs = ((refresh_total_sec / 60) % refresh_mins) * 60;
+
+    lines.push(`Title: ${opts.title}`);
+    if (refresh_mins > 0) lines.push(`Refresh: ${refresh_mins}`);
+    if (refresh_secs > 0) lines.push(`RefreshSeconds: ${refresh_secs}`);
+    if (opts.threshold > 0) lines.push(`Threshold: ${opts.threshold}`);
+    if (opts.defaultColor != undefined) lines.push(`Color: ${opts.defaultColor.join(" ")}`);
+
+    for (const geo of opts.geometry) {
+        if (geo.type == "Line") {
+            if (geo.color != undefined) lines.push(`Color: ${geo.color.join(" ")}`);
+            else if (opts.defaultColor != undefined) lines.push(`Color: ${opts.defaultColor.join(" ")}`);
+            lines.push(`Line: ${geo.width}, 0${geo.hoverText != undefined ? `, "${geo.hoverText.replace(/\n/g, "\\n")}"` : ""}`);
+            lines.push(...geo.points.map(p => p.toString()));
+            lines.push("End:");
+        } else if (geo.type == "Polygon") {
+            lines.push(`Polygon: ${geo.points[0].toString()}, ${geo.color.map(v => `${v}`).join(', ')}`);
+            lines.push(...geo.points.map(p => p.toString()));
+            lines.push("End:");
+        }
+    }
+    return lines.join('\n');
+}
+
+export type IPlaceGeometryBase = {
+    type: "Line" | "Object" | "Polygon" | "Triangle";
+}
+
+export type IPlaceLine = {
+    type: "Line";
+    color?: [number, number, number];
+    width: number;
+    hoverText?: string;
+    points: Vec2[];
+}
+
+export type IPlacePolygon = {
+    type: "Polygon";
+    color: [number, number, number, number];
+    points: Vec2[];
+}
+
+export function rgbToRGBA(rgb: [number, number, number], alpha: number): [number, number, number, number] {
+    return [...rgb, alpha];
+}
+
+export type Line = IPlaceLine & IPlaceGeometryBase;
+export type Polygon = IPlacePolygon & IPlaceGeometryBase;
